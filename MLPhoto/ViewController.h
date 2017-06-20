6f42249df19c3223377fa7ca91c60fe97d4ca135
cendywang <zhaocheng.wang@ele.me>
2017-06-20 22:29:34 +0800

    Init

---
+++ MLPhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MLPhoto
+//
+//  Created by cendywang on 2017/6/20.
+//  Copyright © 2017年 圣迪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MLPhoto/ViewController.m
@@ -0,0 +1,141 @@
+//
+//  ViewController.m
+//  MLPhoto
+//
+//  Created by cendywang on 2017/6/20.
+//  Copyright © 2017年 圣迪. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "MLCameraViewController.h"
+#import <CoreML/CoreML.h>
+#import "Resnet50.h"
+@import CoreVideo;
+@import MobileCoreServices;
+
+@interface ViewController ()<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@property (nonatomic, strong)UIImagePickerController *imagePickerController;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *photoNameLabel;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    UIBarButtonItem *photoBtnItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCamera
+                                                                                  target:self
+                                                                                  action:@selector(takeCamera)];
+    self.navigationItem.rightBarButtonItem = photoBtnItem;
+    
+    self.imagePickerController = [[UIImagePickerController alloc] init];
+    self.imagePickerController.delegate = self;
+    self.imagePickerController.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
+    self.imagePickerController.allowsEditing = YES;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)takeCamera {
+    self.imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+    self.imagePickerController.mediaTypes = @[(NSString *)kUTTypeImage];
+    self.imagePickerController.cameraCaptureMode = UIImagePickerControllerCameraCaptureModePhoto;
+    [self.navigationController presentViewController:self.imagePickerController
+                                            animated:YES
+                                          completion:nil];
+}
+
+#pragma mark -
+#pragma mark - UIImageController Delegate
+- (void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    NSString *mediaType=[info objectForKey:UIImagePickerControllerMediaType];
+    if ([mediaType isEqualToString:(NSString *)kUTTypeImage]){
+        CGSize thesize = CGSizeMake(224, 224);
+        UIImage *theimage = [self image:info[UIImagePickerControllerEditedImage] scaleToSize:thesize];
+        self.imageView.image = theimage;
+        
+        CVPixelBufferRef imageRef = [self pixelBufferFromCGImage:theimage.CGImage];
+        Resnet50 *resnet50Model = [[Resnet50 alloc] init];
+        NSError *error = nil;
+        Resnet50Output *output = [resnet50Model predictionFromImage:imageRef
+                                                              error:&error];
+        if (error == nil) {
+            self.photoNameLabel.text = output.classLabel;
+        } else {
+            NSLog(@"Error is %@", error.localizedDescription);
+        }
+    }
+        
+    UIImagePickerController *imagePickerVC = picker;
+    [imagePickerVC dismissViewControllerAnimated:YES completion:^{
+        
+    }];
+}
+
+#pragma mark - Image Helpful Tools
+- (CVPixelBufferRef) pixelBufferFromCGImage: (CGImageRef) image {
+    NSDictionary *options = @{
+                              (NSString*)kCVPixelBufferCGImageCompatibilityKey : @YES,
+                              (NSString*)kCVPixelBufferCGBitmapContextCompatibilityKey : @YES,
+                              };
+    
+    CVPixelBufferRef pxbuffer = NULL;
+    CVReturn status = CVPixelBufferCreate(kCFAllocatorDefault, CGImageGetWidth(image),
+                                          CGImageGetHeight(image), kCVPixelFormatType_32ARGB, (__bridge CFDictionaryRef) options,
+                                          &pxbuffer);
+    if (status!=kCVReturnSuccess) {
+        NSLog(@"Operation failed");
+    }
+    NSParameterAssert(status == kCVReturnSuccess && pxbuffer != NULL);
+    
+    CVPixelBufferLockBaseAddress(pxbuffer, 0);
+    void *pxdata = CVPixelBufferGetBaseAddress(pxbuffer);
+    
+    CGColorSpaceRef rgbColorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(pxdata, CGImageGetWidth(image),
+                                                 CGImageGetHeight(image), 8, 4*CGImageGetWidth(image), rgbColorSpace,
+                                                 kCGImageAlphaNoneSkipFirst);
+    NSParameterAssert(context);
+    
+    CGContextConcatCTM(context, CGAffineTransformMakeRotation(0));
+    CGAffineTransform flipVertical = CGAffineTransformMake( 1, 0, 0, -1, 0, CGImageGetHeight(image) );
+    CGContextConcatCTM(context, flipVertical);
+    CGAffineTransform flipHorizontal = CGAffineTransformMake( -1.0, 0.0, 0.0, 1.0, CGImageGetWidth(image), 0.0 );
+    CGContextConcatCTM(context, flipHorizontal);
+    
+    CGContextDrawImage(context, CGRectMake(0, 0, CGImageGetWidth(image),
+                                           CGImageGetHeight(image)), image);
+    CGColorSpaceRelease(rgbColorSpace);
+    CGContextRelease(context);
+    
+    CVPixelBufferUnlockBaseAddress(pxbuffer, 0);
+    return pxbuffer;
+}
+
+- (UIImage*)image:(UIImage *)image scaleToSize:(CGSize)size{
+    
+    // 得到图片上下文，指定绘制范围
+    UIGraphicsBeginImageContext(size);
+    
+    // 将图片按照指定大小绘制
+    [image drawInRect:CGRectMake(0, 0, size.width, size.height)];
+    
+    // 从当前图片上下文中导出图片
+    UIImage* scaledImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    // 当前图片上下文出栈
+    UIGraphicsEndImageContext();
+    
+    // 返回新的改变大小后的图片
+    return scaledImage;
+}
+
+@end
